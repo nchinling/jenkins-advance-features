622e6353d22247e1fdd2c80ad5896bedc511cc8e
nchinling <nchinling@gmail.com>
2024-09-01 17:09:30 +0800

    fixed testproperty

--- src/test/java/sg/edu/nus/iss/lovecalculator/LovecalculatorApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/lovecalculator/LovecalculatorApplicationTests.java
@@ -1,10 +1,17 @@
 package sg.edu.nus.iss.lovecalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "LOVE_CALCULATOR_API_URL=http://example.com/api",
+	"LOVE_CALCULATOR_API_KEY=exampekey",
+	"LOVE_CALCULATOR_API_HOST=examplehost"
+
+
+})
 class LovecalculatorApplicationTests {
 
 	@Test
